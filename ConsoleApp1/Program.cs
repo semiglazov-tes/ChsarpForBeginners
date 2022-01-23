ee5f8dbae7c2515a835845ad10eb8084a92bbaf5
semiglazov-tes <semiglazov_tes@mail.ru>
2022-01-23 15:06:51 +0300

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main()
+        {
+ 
+            {
+                Console.WriteLine(item);
+            }
+        }   
+    }
+}
+
