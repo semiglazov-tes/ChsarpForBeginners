9f3007a8132e8cf598d9375413449d5acececc09
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-05 12:37:17 +0300

    Create Calculator.cs
    add class Calculator

---
+++ Lesson_5/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Lesson_5
+{
+    internal class Calculator
+    {
+        private double _firstNumber;
+        private double _secondNumber;
+        private string _operation;
+        private bool _needOperation = true;
+        private bool _correctOperation = false;
+        public double FirstNumber
+        {
+            get
+            {
+                return _firstNumber;
+            }
+            set
+            {
+                _firstNumber = value;
+            }
+        }
+        public double SecondNumber
+        {
+            get
+            {
+                return _secondNumber;
+            }
+            set
+            {
+                _secondNumber = value;
+            }
+        }
+        public string Operation
+        {
+            get
+            {
+                return _operation;
+            }
+            set
+            {
+                _operation = value;
+            }
+        }
+        public bool NeedOperation { get; set; }
+        public bool CorrectOperation { get; set; }
+        public void RunACalculation()
+        {
+            switch (_operation)
+            {
+                case "+":
+                    Console.WriteLine($"Сумма чисел равна:{_firstNumber + _secondNumber}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Разница чисел равна:{_firstNumber - _secondNumber}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Произведение чисел равно:{_firstNumber * _secondNumber}");
+                    break;
+                case "/":
+                    Console.WriteLine($"Результат деления:{_firstNumber / _secondNumber}");
+                    break;
+                default:
+                    Console.WriteLine("Выбрана некорректная операция между двумя числами");
+                    break;
+            }
+        }
+    }
+}
