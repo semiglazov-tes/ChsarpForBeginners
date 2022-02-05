bb059382c3fa6956b4eed24c0a80aa16c7e06177
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-05 12:37:36 +0300

    Create MakeDecisionToProceedWithTheCalculation.cs
    add class MakeDecisionToProceedWithTheCalculation

---
+++ Lesson_5/MakeDecisionToProceedWithTheCalculation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    internal class MakeDecisionToProceedWithTheCalculation
+    {
+        private bool _operationIsCorrect=true;
+        private string _returnOperation;
+        private bool _decisionAboutCalculation;
+        public bool DecisionAboutCalculation
+        {
+            get
+            {
+                return _decisionAboutCalculation;
+            }
+        }
+
+        public void ChekingTheOperation()
+        {         
+            do
+            {
+                Console.Write("Продолжить вычисления(да/нет)?:");
+                _returnOperation = Console.ReadLine().ToLower();
+                if (_returnOperation == "да")
+                {
+                    _decisionAboutCalculation= true;
+                }
+                else if (_returnOperation == "нет")
+                {
+                    _decisionAboutCalculation = false;
+                }
+                else
+                {
+                    Console.WriteLine("Выбрана некорректная операция");
+                    _operationIsCorrect = false;
+                }
+            }
+            while (_operationIsCorrect == false);
+        }
+    }
+}
