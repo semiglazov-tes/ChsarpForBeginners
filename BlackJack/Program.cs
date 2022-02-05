c7829979884995693ed97a45095ae5d1c9402a51
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-05 12:36:11 +0300

    Create Program.cs

---
+++ BlackJack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using BlackJack;
+using static BlackJack.Card;
+
+class Program
+{
+    
+    public static void Main()
+    {
+
+        string[] values = (string[])GetEnumValues();
+        foreach (var color in values)
+        {
+            Console.WriteLine(color);
+        }
+    }
+    public static Array GetEnumValues() 
+    {
+        return Enum.GetNames(typeof(Colors));
+    }
+
+    //How to use
+    public enum Colors
+    {
+        Red,
+        Yellow,
+        Green
+    }
+
+
+}
