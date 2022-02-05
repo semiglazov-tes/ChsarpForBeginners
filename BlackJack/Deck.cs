b75ed8acc7f8a37931f9fa07102fd628eaae2b13
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-05 12:35:50 +0300

    Create Deck.cs
    add class Deck

---
+++ BlackJack/Deck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BlackJack.Card;
+
+
+namespace BlackJack
+{
+    internal class Deck
+    {
+        private Card[] _deck;
+        public Deck() 
+        {
+
+            //CreateDeck();   
+        }
+        public Deck CreateDeck()
+        {
+            string[] suits = (string[])Enum.GetNames(typeof(Suits));
+            string[] rank = (string[])Enum.GetNames(typeof(Suits));
+        }
+    }
+}
+/*public enum Suits
+{
+    Hearts = 3,
+    Diamonds,
+    Clubs,
+    Spades
+}
+public enum Ranks
+{
+    Two = 2,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack = 74,
+    Queen = 81,
+    King = 75,
+    Ace = 65
+}*/
