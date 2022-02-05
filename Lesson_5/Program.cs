bc0976d6499375fc8ddb45a460f403626c7872eb
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-05 12:37:48 +0300

    Create Program.cs

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Lesson_5;
+
+class Program
+{
+    static void Main()
+    {
+        bool needOperation;
+        do
+        {
+            Calculator calculator = new Calculator();
+
+            Console.Write("Введите первое число:");
+            calculator.FirstNumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Определите операцию(+;-;*;/;):");
+            calculator.Operation= Console.ReadLine();
+
+            Console.Write("Введите второе число:");
+            calculator.SecondNumber = Convert.ToDouble(Console.ReadLine());
+
+           calculator.RunACalculation();
+
+            MakeDecisionToProceedWithTheCalculation makeDecision = new MakeDecisionToProceedWithTheCalculation();
+
+            makeDecision.ChekingTheOperation();
+            needOperation =makeDecision.DecisionAboutCalculation;
+        }
+        while (needOperation == true);
+        
+    }
+}
