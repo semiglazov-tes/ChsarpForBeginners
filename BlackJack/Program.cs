9398011cf2ce2d14dbd0fc1b84d5acd820f1ea54
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-06 11:59:09 +0300

    Update Program.cs

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -5,28 +5,12 @@ using static BlackJack.Card;
 
 class Program
 {
-    
+
     public static void Main()
     {
-
-        string[] values = (string[])GetEnumValues();
-        foreach (var color in values)
-        {
-            Console.WriteLine(color);
-        }
+        Deck deck = new Deck();
+        deck.ShuffleTheDeck();
+        deck.DisplayDeck();
+        
     }
-    public static Array GetEnumValues() 
-    {
-        return Enum.GetNames(typeof(Colors));
-    }
-
-    //How to use
-    public enum Colors
-    {
-        Red,
-        Yellow,
-        Green
-    }
-
-
 }
