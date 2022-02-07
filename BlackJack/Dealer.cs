8fdb2471ae85c684006adad7430f74595ba9379a
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-07 21:34:08 +0300

    Create Dealer.cs
    Создал класс Дилера

---
+++ BlackJack/Dealer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    internal class Dealer : Player
+    {
+        public Dealer(string name) : base(name)
+        {
+
+        }
+        public Card IssueACard(Deck deck)
+        {
+           List<Card> listCard=deck.ListCard;
+           Random random = new Random();
+           int randomIndex = random.Next(52);
+           Card issuingCard=listCard[randomIndex];
+           listCard.RemoveAt(randomIndex);
+            return issuingCard;
+        }
+    }
+}
