dedd592ae7815c0caeba6bdeabc86144995020bc
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-13 12:42:02 +0300

    Добавлена реализация поведения дилера и переопределен метод вывода информации об очках и картах у дилера

--- BlackJack/Dealer.cs
+++ BlackJack/Dealer.cs
@@ -8,10 +8,11 @@ namespace BlackJack
 {
     internal class Dealer : Player
     {
-        public Dealer(string name) : base(name)
+        public Dealer(string name="Дилер") : base(name)
         {
 
         }
+        //Реализация выдачи карты из колоды
         public Card IssueACard(Deck deck)
         {
            List<Card> listCard=deck.ListCard;
@@ -20,6 +21,31 @@ namespace BlackJack
            Card issuingCard=listCard[randomIndex];
            listCard.RemoveAt(randomIndex);
             return issuingCard;
+        }
+        //Реализация поведения диалера(брать или не брать карту)
+        public bool DecisionMaking()
+        {
+            Random random = new Random();
+            if (TheSumOfCards <= 10)
+            {
+                return true;
+            }
+            else if (TheSumOfCards > 10 && TheSumOfCards <= 17)
+            {
+                bool decision = Convert.ToBoolean(random.Next(0, 2));
+                return decision;
+            }
+            else { return false; }
+        }
+          //Вывод информации о сумме карт и перечня карт Дилера
+          public override void DisplayPlayerInfo()
+        {
+            Console.WriteLine($"У {Name}a {TheSumOfCards} очков");
+            Console.Write($"Карты {Name}a:");
+            foreach (var card in PlayerDeck)
+            {
+                card.DisplayCard();
+            }
         }
     }
 }
