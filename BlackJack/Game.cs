df8b211e48a27ba5f2814c523f5afd0ff414c5e2
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-13 12:45:05 +0300

    Добавлен класс игры, добавлен метод старт. Начата проработка метода отвечающего за игровой круг

--- BlackJack/Game.cs
+++ BlackJack/Game.cs
@@ -8,5 +8,34 @@ namespace BlackJack
 {
     internal class Game
     {
+        private Deck _gameDeck;
+        private User _user;
+        private Dealer _dealer;
+        public Deck GameDeck { get { return _gameDeck; } set { _gameDeck = value; } }
+        public User user { get { return _user; } set { _user = value; } }
+        public Dealer dealer { get { return _dealer; } set { _dealer = value; } }
+        public void Start()
+        {
+            //создаётся и перемешивается колода
+            GameDeck = new Deck();
+            GameDeck.ShuffleTheDeck();
+  
+            //создание экземляров:Пользователь,Дилер
+            Console.Write("Введите имя игрока:");
+            string UserName = Console.ReadLine();
+            _user = new User(UserName);
+            _dealer = new Dealer(); 
+        }
+        //игровой круг
+        public void FirstHand()
+        {
+            Card issueCardForUser = dealer.IssueACard(GameDeck);
+            user.takeACard(issueCardForUser);
+            Card issueCardForDealer = dealer.IssueACard(GameDeck);
+            dealer.takeACard(issueCardForDealer);
+            user.DisplayPlayerInfo();
+            dealer.DisplayPlayerInfo();
+            //Console.Write("Продолжить брать карты?(да/нет):");
+        }
     }
 }
