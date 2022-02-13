2c20918272d92ac4106de41a46f51973679360e3
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-13 12:42:46 +0300

    Переопределен метод вывода информации об очках и картах пользователя

--- BlackJack/User.cs
+++ BlackJack/User.cs
@@ -8,9 +8,18 @@ namespace BlackJack
 {
     internal class User : Player
     {
-
         public User(string name) : base(name)
         {
+        }
+        //Вывод информации о сумме карт и перечня карт Пользователя
+        public override void DisplayPlayerInfo()
+        {
+            Console.WriteLine($"{Name} у вас {TheSumOfCards} очков");
+            Console.Write("Карты на руках:");
+            foreach (var card in PlayerDeck)
+            {
+                card.DisplayCard();
+            }
         }
     }
 }
