bb417f0f9552fbf0230199b7c2267fecb9330a2f
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-21 22:02:56 +0300

    Update User.cs
    Добавлен метод  DecisionMaking. Добавлен метод DisplayPlayerInfo.

--- BlackJack/User.cs
+++ BlackJack/User.cs
@@ -11,15 +11,44 @@ namespace BlackJack
         public User(string name) : base(name)
         {
         }
-        //Вывод информации о сумме карт и перечня карт Пользователя
+        //принятие решения о продолжении игры
+        public override bool DecisionMaking()
+        {
+        bool _decision = true;
+        string _returnOperation;
+        bool _operationIsCorrect = true;
+         do
+            {
+                Console.Write("Продолжить игру(да/нет)?:");
+                _returnOperation = Console.ReadLine().ToLower();
+                if (_returnOperation == "да")
+                {
+                    _decision=true;
+                    _operationIsCorrect = true;
+                }
+                else if (_returnOperation == "нет")
+                {
+                    _decision=false;
+                    _operationIsCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("Выбрана некорректная операция");
+                    _operationIsCorrect = false;
+                }
+            }
+            while (_operationIsCorrect == false) ;
+            return _decision;   
+        }
+        //Вывод информации о сумме карт и перечня карт Пользователя во время игры
         public override void DisplayPlayerInfo()
         {
-            Console.WriteLine($"{Name} у вас {TheSumOfCards} очков");
-            Console.Write("Карты на руках:");
-            foreach (var card in PlayerDeck)
-            {
-                card.DisplayCard();
-            }
+        Console.WriteLine($"{Name} у вас {TheSumOfCards} очков");
+        Console.Write("Карты на руках:");
+        foreach (var card in PlayerDeck)
+        {
+            card.DisplayCard();
         }
-    }
+        }     
+    }   
 }
