14900f415e99c79ac54e52ad205595c31a27d896
semiglazov-tes <semiglazov_tes@mail.ru>
2022-02-21 21:59:38 +0300

    Update Program.cs
    Модифицирован метод Main

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Text;
 using BlackJack;
-using static BlackJack.Card;
 
 class Program
 {
-
     public static void Main()
     {
-        Deck deck = new Deck();
-        deck.ShuffleTheDeck();
-        Player TestPlayer = new Player("Алексей");
-        Dealer TestDealer = new Dealer("Дилер");
-        Card testCard = TestDealer.IssueACard(deck);
-        TestPlayer.takeACard(testCard);
-        TestPlayer.DisplayPlayerInfo();
-        
+        Game game = new Game();
+        game.Start();
+        do
+        {
+            game.GameCircle();
+        } 
+        while (game.EndGame == false);
+  
     }
 }
